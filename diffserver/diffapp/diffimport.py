c7760994dbe893c55485d26395ee79495fa77a90
Alexey Kryuchkov <alexey.kruchkov@gmail.com>
2013-02-20 21:54:31 +0400

    Импорт коммитов из файлов в модельки + тест

---
+++ diffserver/diffapp/diffimport.py
@@ -0,0 +1,109 @@
+# coding: utf-8
+"""
+    На основе вывода команды git show заполняет модели
+"""
+
+from diffapp.models import Diff, Commit, CommitSequence
+
+
+def make_commit_sequence(git_show_lines):
+    """
+        :param git_show_lines: строчки вывода git show %commit_hash2%..%commit_hash1%
+        :returns: новый, сохраненный в БД объект CommitSequence
+    """
+    if isinstance(git_show_lines, basestring):
+        git_show_lines = git_show_lines.replace('\r\n', '\n').split('\n')
+
+    commit_line_spans = []
+    current_commit_lines = []
+
+    sequence = CommitSequence()
+    sequence.save()
+
+    i = 0
+    while i < len(git_show_lines):
+        if git_show_lines[i].startswith('commit '):
+            if current_commit_lines:
+                commit_line_spans.append(current_commit_lines)
+            current_commit_lines = []
+        current_commit_lines.append(git_show_lines[i])
+        i += 1
+
+    if current_commit_lines:
+        commit_line_spans.append(current_commit_lines)
+
+    # мы получили коммиты в обратном временном порядке.
+    commit_line_spans = commit_line_spans[::-1]
+    commits = [
+        make_commit(commit_line_span, commit_sequence=sequence)
+        for commit_line_span in commit_line_spans
+    ]
+    return sequence
+
+
+def make_commit(lines, commit_sequence=None):
+    """
+        :param lines: строчки вывода git show %commit_hash%
+        :param commit_sequence: пачка коммитов, к которой принадлежит создаваемый коммит
+        :returns: новый, сохраненный в БД объект Commit
+    """
+    assert lines[0].startswith('commit ')
+    sha1 = lines[0].split(' ', 1)[1]
+
+    commit = Commit(commit_sequence=commit_sequence)
+    head = []
+    i = 0
+
+    while i < len(lines):
+        if lines[i].startswith('diff '):
+            break
+        head.append(lines[i].rstrip())
+        i += 1
+
+    commit.head_lines = u'\n'.join(lines)
+    commit.sha1 = sha1
+    commit.save()
+
+    current_diff_lines = []
+
+    while i < len(lines):
+        if lines[i].startswith('diff '):
+            if current_diff_lines:
+                make_diff(current_diff_lines, commit=commit)
+            current_diff_lines = []
+        current_diff_lines.append(lines[i])
+        i += 1
+
+    if current_diff_lines:
+        make_diff(current_diff_lines, commit=commit)
+    return commit
+
+
+def make_diff(lines, commit=None):
+    """
+        :param lines: строчки вывода git diff -u file1 file2 (дифф одного файла)
+        :param commit: коммит, к которому принадлежит создаваемый дифф
+        :returns: новый, сохраненный в БД объект Diff
+    """
+    assert len(lines) > 4
+    assert lines[0].startswith('diff ')
+
+    diff = Diff(commit=commit)
+
+    head = []
+
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith('--- '):
+            diff.filename = lines[i].split(' ', 1)[1]
+        if lines[i].startswith('+++ '):
+            filename = lines[i].split(' ', 1)[1]
+            if filename != '/dev/null':
+                diff.filename = filename
+            break
+        head.append(lines[i].rstrip())
+        i += 1
+    diff.head_lines = u'\n'.join(head)
+    diff.body_lines = u'\n'.join(line.rstrip() for line in lines[i + 1:])
+    diff.save()
+    return diff
--- diffserver/diffapp/tests.py
+++ diffserver/diffapp/tests.py
@@ -1,16 +1,97 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# coding: utf-8
 from django.test import TestCase
+from diffapp.diffimport import make_commit_sequence
+from diffapp.models import CommitSequence, Commit, Diff
 
+EXAMPLE_GIT_SHOW_OUTPUT = u"""commit c28e535f1024e3b22ec05574aa2287aa5338e3dc
+Author: Vasya Pupkin <pupkin.vasily@gmail.com>
+Date:   Wed Feb 20 21:35:38 2013 +0400
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    Оказывается это было в stdlib
+
+diff --git a/a.py b/a.py
+index a788f61..2b10ee5 100644
+--- a/a.py
++++ b/a.py
+@@ -1,3 +1,3 @@
+-import b
+-if b.quaka(x - y):
++import itertools
++if itertools.iterquaka(x - y):
+     return z
+diff --git a/b.py b/b.py
+deleted file mode 100644
+index 6152a79..0000000
+--- a/b.py
++++ /dev/null
+@@ -1,2 +0,0 @@
+-def quaka(x):
+-    return x*x
+
+commit 242ce960af361fd307ca2fe61bc0a71c29c28394
+Author: Vasya Pupkin <pupkin.vasily@gmail.com>
+Date:   Wed Feb 20 21:34:57 2013 +0400
+
+    Рефакторинг
+
+diff --git a/a.py b/a.py
+index d62e9f6..a788f61 100644
+--- a/a.py
++++ b/a.py
+@@ -1,2 +1,3 @@
+-if x = y:
++import b
++if b.quaka(x - y):
+     return z
+diff --git a/b.py b/b.py
+new file mode 100644
+index 0000000..6152a79
+--- /dev/null
++++ b/b.py
+@@ -0,0 +1,2 @@
++def quaka(x):
++    return x*x
+
+commit f60d7b4858faa696c0581f47ccc81736f9df3ded
+Author: Vasya Pupkin <pupkin.vasily@gmail.com>
+Date:   Wed Feb 20 21:34:04 2013 +0400
+
+    Добавили a.py
+
+diff --git a/a.py b/a.py
+new file mode 100644
+index 0000000..d62e9f6
+--- /dev/null
++++ b/a.py
+@@ -0,0 +1,2 @@
++if x = y:
++    return z
+"""
+
+class DiffImportTest(TestCase):
+    """ тесты на импорт коммитов из файла """
+
+    def test_import_commit_sequence(self):
+        make_commit_sequence(EXAMPLE_GIT_SHOW_OUTPUT)
+
+        self.assertEqual(CommitSequence.objects.count(), 1)
+        self.assertEqual(Commit.objects.count(), 3)
+
+        commits = Commit.objects.all()[:]
+
+        self.assertEquals(commits[0].sha1, 'f60d7b4858faa696c0581f47ccc81736f9df3ded')
+        diffs = commits[0].diffs.all()[:]
+        self.assertEquals(len(diffs), 1)
+        self.assertEquals(diffs[0].filename, 'b/a.py')
+
+        self.assertEquals(commits[1].sha1, '242ce960af361fd307ca2fe61bc0a71c29c28394')
+        diffs = commits[1].diffs.all()[:]
+        self.assertEquals(len(diffs), 2)
+        self.assertEquals(diffs[0].filename, 'b/a.py')
+        self.assertEquals(diffs[1].filename, 'b/b.py')
+
+        self.assertEquals(commits[2].sha1, 'c28e535f1024e3b22ec05574aa2287aa5338e3dc')
+        diffs = commits[2].diffs.all()[:]
+        self.assertEquals(len(diffs), 2)
+        self.assertEquals(diffs[0].filename, 'b/a.py')
+        self.assertEquals(diffs[1].filename, 'a/b.py')
